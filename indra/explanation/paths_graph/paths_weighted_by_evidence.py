a962d48016225cbbe30bad491d0f3d023ac6483b
Daniel Milstein <djmilstein@gmail.com>
2018-04-04 10:23:24 -0400

    Compute evidence for a given path

--- indra/explanation/paths_graph/paths_weighted_by_evidence.py
+++ indra/explanation/paths_graph/paths_weighted_by_evidence.py
@@ -31,6 +31,13 @@ class WeightPathsByEvidence(object):
         self._compute_node_evidence()
         self._compute_node_and_children_evidence()
 
+        debug = False
+        if debug:
+            self._debug_output()
+
+    def _debug_output(self):
+        if not debug:
+            return
         for node in self.cfpg.graph.nodes():
             print(node)
             if node in self.node_evidence:
@@ -38,9 +45,41 @@ class WeightPathsByEvidence(object):
             else:
                 print('\tNo node evidence')
             if node in self.node_and_children_evidence:
-                print('\tNode and children:', self.node_and_children_evidence[node])
+                print('\tNode and children:',
+                        self.node_and_children_evidence[node])
             else:
                 print('\tNo node and children evidence')
+
+    def evaluate_path(self, path):
+        """Gives the number of statements supporting the given path.
+
+        Parameters
+        ----------
+        path: list<str>
+            A list of node names in the original graph in the order of path
+            traversal
+
+        Returns
+        -------
+        score: int
+            The number of direct or indirect evidence statements supporting
+            this path
+        """
+        # The path should start at the start node and end at the end node
+        assert(path[0] == self.source_name)
+        assert(path[len(path)-1] == self.target_name)
+
+        path_evidence = 0
+
+        current_node = self._cfpg_source()
+        for ind in range(1, len(path)-1):
+            # Note how there is no node evidence for the source or target nodes
+            # so we do not iterate over those
+            next_name = path[ind]
+            current_node = self._cfpg_next(current_node, next_name)
+            assert(current_node is not None)
+            path_evidence = path_evidence + self.node_evidence[current_node]
+        return path_evidence
 
     def _cfpg_node_history(self, cfpg_node_name):
         """Returns the set of nodes in the history."""
@@ -88,6 +127,16 @@ class WeightPathsByEvidence(object):
                 num_evidence = num_evidence + self.evidence[relation]
         return num_evidence
 
+    def _cfpg_source(self):
+        """Finds the node in the cfpg corresponding to the source node.
+        There should only be one."""
+        source_nodes = []
+        for node in self.cfpg.graph.nodes():
+            if node[1] == self.source_name:
+                source_nodes.append(node)
+        assert(len(source_nodes) == 1)
+        return source_nodes[0]
+
     def _cfpg_target(self):
         """Finds the node in the cfpg corresponding to the target node.
         There should only be one."""
@@ -97,6 +146,37 @@ class WeightPathsByEvidence(object):
                 target_nodes.append(node)
         assert(len(target_nodes) == 1)
         return target_nodes[0]
+
+    def _cfpg_next(self, cfpg_current, original_next):
+        """Finds the node in the cfpg graph corresponding to moving to
+        a new node, with name specified in the original graph.
+
+        Parameters
+        ----------
+        cfpg_start: str
+            The name of the node to move from (in the cfpg graph)
+        original_next: str
+            The name of the next node to go to (name in the original graph)
+
+        Returns
+        -------
+        cfpg_next: str
+            The node name in the cfpg graph corresponding to moving to
+            the node named original_next in the original graph. None if there
+            is no such node in the cfpg graph.
+        """
+        node_cfpg = []
+        for node in cfpg.graph.successors(cfpg_current):
+            if node[1] == original_next:
+                node_cfpg.append(node)
+
+        # There should be at most one way to move to a new node with
+        # a given name
+        assert(len(node_cfpg) <= 1)
+        if len(node_cfpg) == 1:
+            return node_cfpg[0]
+        else:
+            return None
  
     def _compute_node_evidence(self):
         """Computes the supporting evidence for each node of the cfpg."""
@@ -187,3 +267,8 @@ if __name__ == '__main__':
 
     weightedPaths = WeightPathsByEvidence(g_orig, cfpg, evidence)
     node0 = cfpg.graph.nodes()[0]
+
+    paths = cfpg.enumerate_paths()
+    for path in paths:
+        print(path)
+        print(weightedPaths.evaluate_path(path))
